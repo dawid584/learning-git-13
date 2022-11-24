05f3cc5231198ec40b2e4e6f81f27715db703c68
dawid584 <109970770+dawid584@users.noreply.github.com>
2022-11-24 13:28:22 +0100

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -5,37 +5,37 @@ import matplotlib.pyplot as plt
 df= pd.read_csv('movies.csv')
 
 
-quantile = df['vote_average'].quantile(0.75)
+quantile = df['vote_count'].quantile(0.75)
 print(quantile)
-result = df[df['vote_average'] > quantile].sort_values('vote_average',ascending=False).head(10)
+result = df[df['vote_count'] > quantile].sort_values('vote_average',ascending=False).head(10)
 print(result)
 
 
 
 df['release_date'] = pd.to_datetime(df['release_date'])
-data=df.groupby(pd.Grouper(key='release_date',freq='Y')).sum()
-data=data.loc['2010-01-01':'2016-12-31', ['revenue', 'budget']]
 
+
+df["year"] = df.release_date.dt.year
+
+zx=df.groupby("year").agg({"revenue": np.mean, "budget": np.mean})
+chart=zx.loc['2010-01-01':'2017-12-31', ['revenue', 'budget']]
+print(zx)
+        
 
 z=np.arange(3)
 
 def million(z, pos):
-        return 'USD {:20.1f}MLD'.format(z*1e-9)
-
-
-x = np.array([2010, 2011, 2012, 2013, 2014, 2015 , 2016])
+        return 'USD {:5.1f}MLD'.format(z*1e-6)
 
 fig, ax = plt.subplots()
-
-
 formatter = plt.FuncFormatter(million)
 ax.yaxis.set_major_formatter(formatter)
-plt.bar(x,data['budget'],label='budget')
-plt.plot(x,data['revenue'], color='red',label='revenue') # zmieniamy kolor poprzedniej linii
-plt.legend(loc=1)
-plt.xlabel('Lata') # dodajemy opis osi X
-plt.ylabel('Wartość') # dodajemy opis osi Y
-plt.title('Średni przychód i budzet filmów w latach 2010-2016') # dodajemy tytuł wykresu
+chart['budget'].plot(kind='bar')
+chart['revenue'].plot(kind='line',color='red',label='revenue')
+plt.legend(loc=1) 
+plt.xlabel('Lata')  
+plt.ylabel('Wartość')  
+plt.title('Średni przychód i budzet filmów w latach 2010-2016') 
 plt.show()
 
 
@@ -53,5 +53,5 @@ runtime = state_table.pivot_table(values = 'runtime', index = 'genres', aggfunc=
 runtime.sort_values(by='runtime', ascending=False).round(2)
 print(runtime)
 
-runtime['runtime'].hist(bins=30)
+runtime.plot(kind='bar')
 plt.show() 
